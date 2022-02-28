013c5df5f097608f1db63094d86bcc89826f6063
litoninot12345@gmail.com <litoninot12345@gmail.com>
2022-02-28 14:00:26 +0700

    done product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{
+    Product,
+    User
+};
+use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
@@ -11,9 +16,23 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $data = Product::query();        
+
+        if($request->filled("search")){
+            $data->where(function($q) use ($request) {
+                $q->orWhere("title","like","%".$request->search."%")
+                    ->orWhere("price","like","%".$request->search."%")
+                    ->orWhere("stock","like","%".$request->search."%");
+            });
+        }
+
+        $data->orderBy($request->column ?? "id",$request->order ?? "desc");
+
+        $data = $data->paginate($request->per_page ?? 10);
+
+        return response()->json($data);
     }
 
     /**
@@ -22,9 +41,21 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        try{
+            \DB::beginTransaction();
+
+            auth()->user()->products()->create($request->validated());
+
+            \DB::commit();
+            return response()->json([
+                "message" => true
+            ]);
+        }catch(\Exception $e){
+            \DB::rollback();
+            return FormatResponse::failed($e);
+        }
     }
 
     /**
@@ -33,9 +64,9 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
+    public function show(Product $product)
+    {   
+        return response()->json($product);
     }
 
     /**
@@ -45,9 +76,21 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request,Product $product)
     {
-        //
+        try{
+            \DB::beginTransaction();
+
+            $product->update($request->validated());
+
+            \DB::commit();
+            return response()->json([
+                "message" => true
+            ]);
+        }catch(\Exception $e){
+            \DB::rollback();
+            return FormatResponse::failed($e);
+        }   
     }
 
     /**
@@ -56,8 +99,20 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Product $product)
     {
-        //
+        try{
+            \DB::beginTransaction();
+
+            $product->delete();
+
+            \DB::commit();
+            return response()->json([
+                "message" => true
+            ]);
+        }catch(\Exception $e){
+            \DB::rollback();
+            return FormatResponse::failed($e);
+        }
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,8 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
