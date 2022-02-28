9a1f554b917c091be025959f8fdb84579e3b3f0c
litoninot12345@gmail.com <litoninot12345@gmail.com>
2022-02-28 13:46:59 +0700

    done auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    AuthController,
+    ProfilController,
+    ProductController
+};  
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +18,32 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get("/status",function(){
+    return response()->json([
+        "status" => "Active"
+    ]);
+});
+
+Route::group(["as" => "auth."],function(){
+    Route::post("/signup",[AuthController::class,"signup"])->name("signup");
+    Route::post("/signin",[AuthController::class,"signin"])->name("signin");
+
+    Route::post("/forgot-password",[AuthController::class,"forgotPassword"])->name("forgot-password");
+    Route::post("/reset-password",[AuthController::class,"resetPassword"])->name("reset-password");
+
+    Route::group(["middleware" => "auth:sanctum"],function(){
+        Route::post("/logout",[AuthController::class,'logout'])->name("logout");
+        Route::get("/me",[AuthController::class,'me'])->name("me");
+    });
+});
+
+Route::group(["middleware" => ["auth:sanctum"]],function(){
+    Route::group(["prefix" => "profil","as" => "profil."],function(){
+        Route::post("/data",[ProfilController::class,"updateData"])->name("update.data");
+        Route::post("/photo",[ProfilController::class,"updatePhoto"])->name("update.photo");
+        Route::post("/password",[ProfilController::Class,"updatePassword"])->name("update.password");
+    });
+
+    Route::apiResource("product",ProductController::class);
+});
